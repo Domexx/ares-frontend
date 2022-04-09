d4196544300ffb84b51d2f3655f8c6052891fcb8
Domexx <dominik-forschner@web.de>
2022-04-09 22:02:23 +0200

    [TASK] Fix linting errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
 export default App;
