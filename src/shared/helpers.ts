408425b89854464d43a182dc04b0a05fda3f160b
Domexx <dominik-forschner@web.de>
2022-04-14 23:18:17 +0200

    [TASK] Formatted helpers

--- src/shared/helpers.ts
+++ src/shared/helpers.ts
@@ -6,15 +6,15 @@
  * and add the proper security attributes.
  */
 export const isExternalLink = (urlString: string) => {
-    try {
-        const url = new URL(urlString);
+  try {
+    const url = new URL(urlString);
 
-        if (url.origin !== new URL(document.URL, document.baseURI).origin) {
-            return true;
-        }
-    } catch (e) {
-        // We don't care about the error
+    if (url.origin !== new URL(document.URL, document.baseURI).origin) {
+      return true;
     }
+  } catch (e) {
+    // We don't care about the error
+  }
 
-    return false;
+  return false;
 };
