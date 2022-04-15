10a17141dc0355c7e18205f9d696f58312cf08c6
Domexx <dominik-forschner@web.de>
2022-04-15 15:15:22 +0200

    [TASK] Updated snapshot file for navigation

--- src/components/Navigation/Navigation.spec.tsx
+++ src/components/Navigation/Navigation.spec.tsx
@@ -4,9 +4,19 @@ import { render } from '@testing-library/react';
 
 import Navigation from './Navigation';
 
+import Menu from './Navigation.mock.json';
+import { MenuItem } from '../../shared/interfaces/MenuItem';
+
 describe('Navigation Component', () => {
+  let mainMock: MenuItem[];
+
+  beforeAll(() => {
+    const { main } = Menu;
+    mainMock = main;
+  });
+
   it('should render properly', () => {
-    const { container } = render(<Navigation />);
+    const { container } = render(<Navigation main={mainMock} />);
 
     expect(container.firstChild).toMatchSnapshot();
   });
