522b93bd871e05122f3d3f243d482d3ecd4476b1
Dome <54440915+Domexx@users.noreply.github.com>
2022-04-20 23:27:09 +0200

    [TASK] Added possibility to easy create SEO configs (#11)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { DefaultSeo } from 'next-seo';
+import SEO from '../../next-seo.config';
+
 import '../assets/css/main.css';
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
