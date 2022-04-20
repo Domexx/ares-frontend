eb9d643e4ce0a96dac24e3f61dee79436f3cb5a0
Domexx <dominik-forschner@web.de>
2022-04-20 23:03:25 +0200

    [TASK] Added basic layout to wrap pages with

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
+import Layout from '@Cosmic/components/Layout';
+
 export default function Index() {
-  return <h1>Works</h1>;
+  return (
+    <Layout title="Dashboard">
+      <h1>Whatever</h1>
+      <h1>Whatever</h1>
+      <h1>Whatever</h1>
+      <h1>Whatever</h1>
+      <h1>Whatever</h1>
+      <h1>Whatever</h1>
+      <h1>Whatever</h1>
+      <h1>Whatever</h1>
+      <h1>Whatever</h1>
+      <h1>Whatever</h1>
+      <h1>Whatever</h1>
+      <h1>Whatever</h1>
+      <h1>Whatever</h1>
+      <h1>Whatever</h1>
+    </Layout>
+  );
 }
