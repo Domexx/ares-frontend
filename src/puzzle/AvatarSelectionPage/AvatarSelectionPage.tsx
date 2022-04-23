feb824007d142c0715fb8f1e8d095ba4a39f3e01
Raizer <bryanvdstarre@gmail.com>
2022-04-23 22:42:26 +0200

    [TASK] Removed unecessary file and adjusted import

--- src/puzzle/AvatarSelectionPage/AvatarSelectionPage.tsx
+++ src/puzzle/AvatarSelectionPage/AvatarSelectionPage.tsx
@@ -1,4 +1,4 @@
-import react from 'react;';
+import react from 'react';
 import AvatarSelector, { Props as AvatarSelectionProps } from '../../components/AvatarSelector/AvatarSelector';
 
 export type Props = {
