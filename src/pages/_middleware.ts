ae07bb34df521ee884495c304347faf5a4bdd67c
Dome <dominik-forschner@web.de>
2022-06-02 21:01:43 +0200

    [TASK] Added middleware with new url mechanic

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export default function Middleware(req: NextRequest) {
+  // Clone the URL
+  const url = req.nextUrl.clone();
+  const xurl: string = req.url;
+  const baseUrl: string = req.nextUrl.origin;
+  const { pathname, basePath } = req.nextUrl;
+
+  // Simulate connection with a redis cache
+  const isInMaintenanceMode = process.env.NEXT_PUBLIC_MAINTENANCE_MODE;
+
+  const unprotectedPaths: string[] = [
+    `${baseUrl}/static/images/Flags/GER.svg`,
+    `${baseUrl}/static/images/Flags/BR.svg`,
+    `${baseUrl}/static/images/Flags/NL.svg`,
+    `${baseUrl}/static/images/Flags/TR.svg`,
+    `${baseUrl}/static/images/Flags/US.svg`,
+    `/favicon.ico`,
+    `/_next/webpack-hmr`,
+  ];
+
+  // If is in maintenance mode, point the url pathname to the maintenance page
+  if (isInMaintenanceMode && !unprotectedPaths.includes(xurl)) {
+    url.pathname = `/maintenance`;
+
+    // Rewrite to the url
+    return NextResponse.rewrite(new URL(`${basePath}/${pathname}`, req.nextUrl.origin).toString());
+  }
+
+  return NextResponse.next();
+}
