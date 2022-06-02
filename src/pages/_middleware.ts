c4ee466c5562acb84aa4cda44766873d2d75d481
Dome <dominik-forschner@web.de>
2022-06-02 21:49:00 +0200

    [TASK] Added locale and image forwarding to middleware

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -3,29 +3,21 @@ import { NextRequest, NextResponse } from 'next/server';
 export default function Middleware(req: NextRequest) {
   // Clone the URL
   const url = req.nextUrl.clone();
-  const xurl: string = req.url;
-  const baseUrl: string = req.nextUrl.origin;
+
+  url.searchParams.set('locale', url.locale)
 
   // Simulate connection with a redis cache
   const isInMaintenanceMode = process.env.NEXT_PUBLIC_MAINTENANCE_MODE;
 
-  const unprotectedPaths: string[] = [
-    `${baseUrl}/static/images/Flags/GER.svg`,
-    `${baseUrl}/static/images/Flags/BR.svg`,
-    `${baseUrl}/static/images/Flags/NL.svg`,
-    `${baseUrl}/static/images/Flags/TR.svg`,
-    `${baseUrl}/static/images/Flags/US.svg`,
-    `/favicon.ico`,
-    `/_next/webpack-hmr`,
-  ];
+  if (url.pathname.includes(".")) {
+    return undefined;
+  }
 
   // If is in maintenance mode, point the url pathname to the maintenance page
-  if (isInMaintenanceMode && !unprotectedPaths.includes(xurl)) {
+  if (isInMaintenanceMode) {
     url.pathname = `/maintenance`;
-    url.searchParams.append('callback', new URL(req.url).pathname);
-
     // Rewrite to the url
-    return NextResponse.redirect(url);
+    return NextResponse.rewrite(url);
   }
 
   return NextResponse.next();
